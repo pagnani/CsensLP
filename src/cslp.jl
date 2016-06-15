fbfd969d2a1db02bc83af0f28abfc26636d5afa0
pagnani <andrea.pagnani@gmail.com>
2016-06-15 11:38:00 +0200

    changed module name again

--- src/cslp.jl
+++ src/cslp.jl
@@ -1,4 +1,4 @@
-module CSlp
+module CsensLp
 # given y,A  find x s.t 1)  y = A * x , 2)  |x|_1 is minimal 
 # gurobi and jump version should solve (it is not clear)
 # given y,A,λ  find  argmin_x { ||y - A * x||_∞ , + λ |x|_1 }
