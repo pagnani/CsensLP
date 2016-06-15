3df4620f3eff62bcf3af54c53cc073fd4baeb357
pagnani <andrea.pagnani@gmail.com>
2016-06-15 13:39:07 +0200

    fixed typo in module name

--- src/CsensLP.jl
+++ src/CsensLP.jl
@@ -1,13 +1,14 @@
-module CsensLp
+module CsensLP
+
+using Gurobi
+using JuMP
+using Convex
 
 # given y,A  find x s.t 1)  y = A * x , 2)  |x|_1 is minimal 
 # gurobi and jump version should solve (it is not clear)
 # given y,A,λ  find  argmin_x { ||y - A * x||_∞ , + λ |x|_1 }
 
-using Gurobi,JuMP,Convex
-
-export CSOut, RunOut, ResOut, cslconvex, cslgurobi, csljump, compute_roc, runanal, analdata
-
+export CSOut, RunOut, ResOut, cslconvex, cslgurobi, csljump, compute_roc, runanal, analdata, alphac
 
 include("types.jl")
 include("cs.jl")
